7f4cf260411ded42daf2aab2be73ef27965fc4c4
Tanzhor_RIK <tanzhor4@gmail.com>
2023-03-29 01:42:13 +0700

    add f_capitalize

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def f(s:str)->str.upper:
     """This function return string.upper"""
     return s.upper()
+
+
+def f_capitalize(s:str)->str.capitalize:
+    """This function return string.capitalize"""
+    return s.capitalize()
